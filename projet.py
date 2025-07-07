4030237bd17f2115dab0147c6b4f744c9d8cd6af
Mohamed-Hedi-Gharbi <mohamedhedigharbi101@gmail.com>
2025-07-07 18:48:46 +0100

    Fusion squashed de project1: Ajout et modifications de projet.py

--- projet.py
+++ projet.py
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 def produit (T):
     S = 0
     for i n T:
     	S++=t
     	return S
+=======
+Data = [1,3,5]
+if Data:
+	print("la somme est :", sum(Data))
+	print("le min est :", min(Data))
+	print("le max est :", max(Data))
+>>>>>>> project1
